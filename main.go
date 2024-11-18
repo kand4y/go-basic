2d0769d83723762585e6ca5b5b25045d4ad5ad37
kand4y <kand4y@gmail.com>
2024-11-18 23:56:57 +0900

    add: fizz_buzz関数を追加

--- main.go
+++ main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World")
+
+	fizz_buzz()
+}
+
+func fizz_buzz() {
+	for i := 1; i < 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("Fizz Buzz!!")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz!!")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz!!")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
